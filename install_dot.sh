ebb51a6ea1e98b46af38514c4dcac499078a54bc
Geoff Webster <gwebster@aurora.tech>
2021-12-28 12:37:32 -0500

    Add a Terraformrc config to setup a plugin cache.

--- install_dot.sh
+++ install_dot.sh
@@ -9,3 +9,7 @@ ln -sf ~/dotfiles/.zshrc $HOME/.zshrc
 
 echo "INSTALL_DOT: Symlinking init.vim"
 ln -sf ~/dotfiles/init.vim $HOME/.config/nvim/init.vim
+
+# dotfiles folder must exist in $HOME
+echo "INSTALL_DOT: Symlinking .zshrc"
+ln -sf ~/dotfiles/.terraformrc $HOME/.terraformrc
